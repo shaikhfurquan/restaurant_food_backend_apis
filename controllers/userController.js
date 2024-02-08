1abbc86ee5320d44765f25a09ba7d79b77ee8aad
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-02-08 23:58:30 +0530

    register ctrl

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,34 @@
+import UserModel from "../models/userModel.js"
 
-export const userRegister = async(req, res) =>{
+export const userRegister = async (req, res) => {
     try {
-        
+        const { userName, email, password, phone, address } = req.body
+        if (!username || !email || !password || !phone || !address) {
+            return res.status(500).json({
+                succcess: false,
+                message: "Please enter provide all fields"
+            })
+        }
+        const existingUser = await UserModel.findOne({ email })
+        if (!existingUser) {
+            return res.status(500).json({
+                success: false,
+                message : "Email already exists please login"
+            })
+        }
+
+        const user = await UserModel.create({
+            userName, email, password, address, phone
+        })
+        res.status(201).json({
+            success : true,
+            message : "User created successfully",
+            user : user
+        })
     } catch (error) {
         res.status(404).json({
             succcess: false,
-            message : "Error registering user",
+            message: "Error registering user",
             error: error.message
         })
     }
