ec30284e3266527e6f36265abe6456d7268d2f0a
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-02-08 23:59:24 +0530

    login route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { userRegister } from '../controllers/userController.js';
+import { userLogin, userRegister } from '../controllers/userController.js';
 
 const userRouter = express.Router()
 
 userRouter.post('/register', userRegister)
+userRouter.post('/login', userLogin)
 
 export default userRouter
