eb2981122a372df6a9197761985477b1a05a259a
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-02-09 13:41:09 +0530

    update dbName

--- db/connectDB.js
+++ db/connectDB.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () =>{
     try {
-        await mongoose.connect(process.env.MONGO_URL)
-        console.log(`connected to db successfully ==> ${process.env.MONGO_URL} `);
+        await mongoose.connect(process.env.MONGO_URL + process.env.DB_NAME)
+        console.log(`connected to db successfully ==> ${process.env.MONGO_URL + process.env.DB_NAME} `);
     } catch (error) {
         console.log('Error connecting to DB ==> ' , error.message);
     }  
