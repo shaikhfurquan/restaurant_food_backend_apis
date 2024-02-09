d9b9a81333a9efbf11af14f918d9db072a443119
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-02-09 13:40:20 +0530

    update register ctrl

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,38 +3,40 @@ import UserModel from "../models/userModel.js"
 export const userRegister = async (req, res) => {
     try {
         const { userName, email, password, phone, address } = req.body
-        if (!username || !email || !password || !phone || !address) {
-            return res.status(500).json({
-                succcess: false,
-                message: "Please enter provide all fields"
-            })
-        }
-        const existingUser = await UserModel.findOne({ email })
-        if (!existingUser) {
+        if (!userName || !email || !password || !phone || !address) {
             return res.status(500).json({
                 success: false,
-                message : "Email already exists please login"
+                message: "All fields are required"
             })
         }
 
-        const user = await UserModel.create({
-            userName, email, password, address, phone
-        })
+        //checking whether the user is already is registered or not
+        const existingUser = await UserModel.findOne({ email })
+        if (existingUser) {
+            return res.status(500).json({
+                success: false,
+                message: "User already exists, Please Login"
+            })
+        }
+
+        //create new user
+        const user = await UserModel.create({ userName, email, password, phone, address })
+
         res.status(201).json({
-            success : true,
-            message : "User created successfully",
-            user : user
+            success: true,
+            message: "User created successfully",
+            user: user
         })
     } catch (error) {
         res.status(404).json({
             succcess: false,
-            message: "Error registering user",
+            message: "Error while registering user",
             error: error.message
         })
     }
-} 
+}
 
 
 export const userLogin = (req, res) => {
-    
+
 }
