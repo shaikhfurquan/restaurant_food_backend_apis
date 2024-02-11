ccd442b4f40da349d98f83635b2d50d9dc63d6a4
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-02-11 12:35:59 +0530

    update user

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { userLogin, userRegister } from '../controllers/userController.js';
+import { getAUser, updateUser, userLogin, userRegister } from '../controllers/userController.js';
+import { isAuthenticated } from '../middlewares/isAuthenticate.js';
 
 const userRouter = express.Router()
 
 userRouter.post('/register', userRegister)
 userRouter.post('/login', userLogin)
+
+userRouter.get('/getuser', isAuthenticated , getAUser)
+userRouter.put('/updateuser', isAuthenticated , updateUser)
 
 export default userRouter
