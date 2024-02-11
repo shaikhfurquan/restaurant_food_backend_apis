bbcc43a7da30d73ff4d73cb3f1e0a33c275c50a8
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-02-12 00:28:28 +0530

    update/reset pswd route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getAUser, updateUser, userLogin, userRegister } from '../controllers/userController.js';
+import { getAUser, updateUser, userLogin, userRegister , resetPassword, updatePassword } from '../controllers/userController.js';
 import { isAuthenticated } from '../middlewares/isAuthenticate.js';
 
 const userRouter = express.Router()
@@ -9,5 +9,9 @@ userRouter.post('/login', userLogin)
 
 userRouter.get('/getuser', isAuthenticated , getAUser)
 userRouter.put('/updateuser', isAuthenticated , updateUser)
+
+
+userRouter.post('/reset-password', isAuthenticated , resetPassword)
+userRouter.post('/update-password', isAuthenticated , updatePassword)
 
 export default userRouter
