6c8453c8b54ce5269730de1b09abc77e1c324463
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-02-13 13:04:45 +0530

    get rerestaurant

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -28,3 +28,55 @@ export const createRestaurant = async (req, res) => {
         })
     }
 }
+
+
+export const getAllRestaurants = async (req, res) => {
+    try {
+        const allRestaurants = await RestaurantModel.find()
+        if(!allRestaurants){
+            return res.status(404).json({
+                success: false,
+                message: "Restaurants not found"
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            message : "Restaurants Lists",
+            totalCount : allRestaurants.length,
+            allRestaurants: allRestaurants
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Error while getting restaurants lists",
+            error: error.message
+        })
+    }
+}
+
+
+export const getRestaurantById = async (req, res) => {
+    try {
+        const restaurantId = req.params.id
+        const restaurant = await RestaurantModel.findById(restaurantId)
+        if(!restaurant){
+            return res.status(404).json({
+                success: false,
+                message: "Restaurant not found with this Id"
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            message : "Restaurant",
+            restaurant : restaurant
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Error while getting restaurant",
+            error: error.message
+        })
+    }
+}
--- routes/restaurantRoute.js
+++ routes/restaurantRoute.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { isAuthenticated } from '../middlewares/isAuthenticate.js';
-import { createRestaurant } from '../controllers/restaurantController.js';
+import { createRestaurant, getAllRestaurants, getRestaurantById } from '../controllers/restaurantController.js';
 
 const restaurantRouter = express.Router();
 
 
 restaurantRouter.post('/create' , isAuthenticated , createRestaurant)
+restaurantRouter.get('/get-all' , getAllRestaurants)
+restaurantRouter.get('/get/:id' , getRestaurantById)
 
 
 export default restaurantRouter
