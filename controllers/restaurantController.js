ee4487d6cf7924e977deecc86eea797ef49344be
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-02-13 14:01:22 +0530

    delete restaurant

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -6,19 +6,19 @@ export const createRestaurant = async (req, res) => {
     try {
         const { title, foods, pickup, delivery, isOpen, rating, imageUrl, time, logoUrl, ratingCount, code, coords } = req.body
 
-        if(!title || !coords){
+        if (!title || !coords) {
             return res.status(500).json({
                 success: false,
                 message: "Please provide a title and address"
             })
         }
 
-        const newRestaurant = await RestaurantModel.create({title, foods, pickup, delivery, isOpen, rating, imageUrl, time, logoUrl, ratingCount, code, coords})
+        const newRestaurant = await RestaurantModel.create({ title, foods, pickup, delivery, isOpen, rating, imageUrl, time, logoUrl, ratingCount, code, coords })
 
         res.status(200).json({
             success: true,
             message: "Restaurant created successfully",
-            newRestaurant : newRestaurant
+            newRestaurant: newRestaurant
         })
     } catch (error) {
         res.status(500).json({
@@ -33,7 +33,7 @@ export const createRestaurant = async (req, res) => {
 export const getAllRestaurants = async (req, res) => {
     try {
         const allRestaurants = await RestaurantModel.find()
-        if(!allRestaurants){
+        if (!allRestaurants) {
             return res.status(404).json({
                 success: false,
                 message: "Restaurants not found"
@@ -42,8 +42,8 @@ export const getAllRestaurants = async (req, res) => {
 
         res.status(200).json({
             success: true,
-            message : "Restaurants Lists",
-            totalCount : allRestaurants.length,
+            message: "Restaurants Lists",
+            totalCount: allRestaurants.length,
             allRestaurants: allRestaurants
         })
     } catch (error) {
@@ -60,17 +60,50 @@ export const getRestaurantById = async (req, res) => {
     try {
         const restaurantId = req.params.id
         const restaurant = await RestaurantModel.findById(restaurantId)
-        if(!restaurant){
+        if (!restaurant) {
             return res.status(404).json({
                 success: false,
-                message: "Restaurant not found with this Id"
+                message: "Restaurant not found"
             })
         }
 
         res.status(200).json({
             success: true,
-            message : "Restaurant",
-            restaurant : restaurant
+            message: "Restaurant",
+            restaurant: restaurant
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Error while getting restaurant",
+            error: error.message
+        })
+    }
+}
+
+
+
+export const deleteRestaurantById = async (req, res) => {
+    try {
+        const restaurantId = req.params.id
+        if (!restaurantId) {
+            return res.status(404).json({
+                success: false,
+                message: "Please provide restaurant Id"
+            })
+        }
+
+        const deletedRestaurant = await RestaurantModel.findByIdAndDelete(restaurantId)
+        if (!deletedRestaurant) {
+            return res.status(404).json({
+                success: false,
+                message: `Restaurant with this Id not found.`,
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Restaurant Deleted Successfully",
         })
     } catch (error) {
         res.status(500).json({
--- routes/restaurantRoute.js
+++ routes/restaurantRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { isAuthenticated } from '../middlewares/isAuthenticate.js';
-import { createRestaurant, getAllRestaurants, getRestaurantById } from '../controllers/restaurantController.js';
+import { createRestaurant, deleteRestaurantById, getAllRestaurants, getRestaurantById } from '../controllers/restaurantController.js';
 
 const restaurantRouter = express.Router();
 
@@ -8,6 +8,7 @@ const restaurantRouter = express.Router();
 restaurantRouter.post('/create' , isAuthenticated , createRestaurant)
 restaurantRouter.get('/get-all' , getAllRestaurants)
 restaurantRouter.get('/get/:id' , getRestaurantById)
+restaurantRouter.delete('/delete/:id' , isAuthenticated , deleteRestaurantById)
 
 
 export default restaurantRouter
