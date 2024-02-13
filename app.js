742c0399ff61b682efeeda281199f3fad80f7489
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-02-14 00:54:39 +0530

    import category route

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import morgan from 'morgan';
 import connectDB from './db/connectDB.js';
 import userRouter from './routes/userRoute.js';
 import restaurantRouter from './routes/restaurantRoute.js';
+import categoryRouter from './routes/categoryRoute.js';
 
 
 dotenv.config()
@@ -21,6 +22,7 @@ app.use(morgan('dev'))
 //routes
 app.use('/api/user' , userRouter)
 app.use('/api/restaurant' , restaurantRouter)
+app.use('/api/category' , categoryRouter)
 
 //db connection
 connectDB()
